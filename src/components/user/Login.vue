173ded7a1481309c1f1f794f72249b26f3e63748
Saadin <hyphensaad@gmail.com>
2025-04-10 15:31:46 +0500

    updated

--- src/components/user/Login.vue
+++ src/components/user/Login.vue
@@ -12,106 +12,147 @@
  See the License for the specific language governing permissions and
  limitations under the License.-->
 
-
 <template>
-<div class="panel">
-  <template v-if="forgotUsername">
-    <p class="panel-heading">
-      <i class="fas fa-user" aria-hidden="true"></i>
-      {{$t('forgot-username')}}
-      <a href="https://doc.cytomine.org" target="_blank"><img src="@/assets/logo.svg" id="logo" alt="Cytomine"></a>
-    </p>
-    <div class="panel-block">
-      <form @submit.prevent="sendUsername">
-        <b-field :label="$t('email')">
-          <b-input v-model="email" />
-        </b-field>
+  <div class="panel">
+    <template v-if="forgotUsername">
+      <p class="panel-heading">
+        <i class="fas fa-user" aria-hidden="true"></i>
+        {{ $t("forgot-username") }}
+        <a href="https://doc.cytomine.org" target="_blank"
+          ><img src="@/assets/logo.svg" id="logo" alt="Cytomine"
+        /></a>
+      </p>
+      <div class="panel-block">
+        <form @submit.prevent="sendUsername">
+          <b-field :label="$t('email')">
+            <b-input v-model="email" />
+          </b-field>
 
-        <div class="buttons is-right">
-          <button class="button" type="button" @click="forgotUsername = false"> {{$t('button-cancel')}}</button>
-          <button class="button is-link"> {{$t('button-get-username')}}</button>
-        </div>
-      </form>
-    </div>
-  </template>
-
-  <template v-else-if="forgotPassword">
-    <p class="panel-heading">
-      <i class="fas fa-user" aria-hidden="true"></i>
-      {{$t('forgot-password')}}
-      <a href="https://doc.cytomine.org" target="_blank"><img src="@/assets/logo.svg" id="logo" alt="Cytomine"></a>
-    </p>
-    <div class="panel-block">
-      <form @submit.prevent="resetPassword()">
-        <b-field :label="$t('username')">
-          <b-input v-model="username" />
-        </b-field>
-
-        <div class="buttons is-right">
-          <button class="button" type="button" @click="forgotPassword = false"> {{$t('button-cancel')}}</button>
-          <button class="button is-link"> {{$t('button-reset-password')}}</button>
-        </div>
-      </form>
-    </div>
-  </template>
-
-  <register v-else-if="registering" @close="registering=false" />
-
-  <template v-else>
-    <p class="panel-heading">
-      <i class="fas fa-user" aria-hidden="true"></i>
-      {{$t('login')}}
-      <a href="https://doc.cytomine.org" target="_blank"><img src="@/assets/logo.svg" id="logo" alt="Cytomine"></a>
-    </p>
-    <div class="panel-block">
-      <form @submit.prevent="login()">
-        <b-field :label="$t('username')">
-          <b-input v-model="username" />
-        </b-field>
-
-        <b-field :label="$t('password')">
-          <b-input type="password" v-model="password" />
-        </b-field>
-
-        <div class="columns">
-          <div class="column">
-            <b-field>
-              <b-checkbox v-model="rememberMe" type="is-info">{{$t('remember-me')}}</b-checkbox>
-            </b-field>
+          <div class="buttons is-right">
+            <button
+              class="button"
+              type="button"
+              @click="forgotUsername = false"
+            >
+              {{ $t("button-cancel") }}
+            </button>
+            <button class="button is-link">
+              {{ $t("button-get-username") }}
+            </button>
           </div>
+        </form>
+      </div>
+    </template>
 
-          <div class="column has-text-right">
-            <button class="button is-link"> {{$t('button-login')}}</button>
+    <template v-else-if="forgotPassword">
+      <p class="panel-heading">
+        <i class="fas fa-user" aria-hidden="true"></i>
+        {{ $t("forgot-password") }}
+        <a href="https://doc.cytomine.org" target="_blank"
+          ><img src="@/assets/logo.svg" id="logo" alt="Cytomine"
+        /></a>
+      </p>
+      <div class="panel-block">
+        <form @submit.prevent="resetPassword()">
+          <b-field :label="$t('username')">
+            <b-input v-model="username" />
+          </b-field>
+
+          <div class="buttons is-right">
+            <button
+              class="button"
+              type="button"
+              @click="forgotPassword = false"
+            >
+              {{ $t("button-cancel") }}
+            </button>
+            <button class="button is-link">
+              {{ $t("button-reset-password") }}
+            </button>
           </div>
-        </div>
-      </form>
-    </div>
-    <i18n path="forgot-credentials" tag="p" class="is-size-7 has-text-centered forgot-credentials">
-      <a place="username" @click="forgotUsername = true">{{ $t('username') }}</a>
-      <a place="password" @click="forgotPassword = true">{{ $t('password') }}</a>
-    </i18n>
-    <i18n v-if="allowedRegistration" path="no-account-yet" tag="p" class="is-size-7 has-text-centered">
-      <a place="registerLink" @click="registering = true">{{$t('register')}}</a>
-    </i18n>
-  </template>
-</div>
+        </form>
+      </div>
+    </template>
+
+    <register v-else-if="registering" @close="registering = false" />
+
+    <template v-else>
+      <p class="panel-heading">
+        UNDER CONSTRUCTION
+      </p>
+      <p class="panel-heading">
+        <i class="fas fa-user" aria-hidden="true"></i>
+        {{ $t("login") }}
+        <a href="https://doc.cytomine.org" target="_blank"
+          ><img src="@/assets/logo.svg" id="logo" alt="Cytomine"
+        /></a>
+      </p>
+      <div class="panel-block">
+        <form @submit.prevent="login()">
+          <b-field :label="$t('username')">
+            <b-input v-model="username" />
+          </b-field>
+
+          <b-field :label="$t('password')">
+            <b-input type="password" v-model="password" />
+          </b-field>
+
+          <div class="columns">
+            <div class="column">
+              <b-field>
+                <b-checkbox v-model="rememberMe" type="is-info">{{
+                  $t("remember-me")
+                }}</b-checkbox>
+              </b-field>
+            </div>
+
+            <div class="column has-text-right">
+              <button class="button is-link">{{ $t("button-login") }}</button>
+            </div>
+          </div>
+        </form>
+      </div>
+      <i18n
+        path="forgot-credentials"
+        tag="p"
+        class="is-size-7 has-text-centered forgot-credentials"
+      >
+        <a place="username" @click="forgotUsername = true">{{
+          $t("username")
+        }}</a>
+        <a place="password" @click="forgotPassword = true">{{
+          $t("password")
+        }}</a>
+      </i18n>
+      <i18n
+        v-if="allowedRegistration"
+        path="no-account-yet"
+        tag="p"
+        class="is-size-7 has-text-centered"
+      >
+        <a place="registerLink" @click="registering = true">{{
+          $t("register")
+        }}</a>
+      </i18n>
+    </template>
+  </div>
 </template>
 
 <script>
-import {get} from '@/utils/store-helpers';
-import {changeLanguageMixin} from '@/lang.js';
-import {Cytomine} from 'cytomine-client';
-import Register from './Register';
+import { get } from "@/utils/store-helpers";
+import { changeLanguageMixin } from "@/lang.js";
+import { Cytomine } from "cytomine-client";
+import Register from "./Register";
 
 export default {
-  name: 'login',
-  components: {Register},
+  name: "login",
+  components: { Register },
   mixins: [changeLanguageMixin],
   data() {
     return {
-      username: '',
-      password: '',
-      email: '',
+      username: "",
+      password: "",
+      email: "",
       rememberMe: true,
 
       allowedRegistration: false, // TODO: retrieve info from core
@@ -122,47 +163,61 @@ export default {
     };
   },
   computed: {
-    currentUser: get('currentUser/user')
+    currentUser: get("currentUser/user")
   },
   methods: {
     async login() {
       try {
-        await this.$store.dispatch('currentUser/login', {
+        await this.$store.dispatch("currentUser/login", {
           username: this.username,
           password: this.password,
           rememberMe: this.rememberMe
         });
-        if(this.currentUser) {
+        if (this.currentUser) {
           this.changeLanguage(this.currentUser.language);
-          this.$notify({type: 'success', text: this.$t('notif-success-login')});
+          this.$notify({
+            type: "success",
+            text: this.$t("notif-success-login")
+          });
+        } else {
+          this.$notify({
+            type: "error",
+            text: this.$t("notif-unexpected-error")
+          });
         }
-        else {
-          this.$notify({type: 'error', text: this.$t('notif-unexpected-error')});
-        }
-      }
-      catch(error) {
+      } catch (error) {
         console.log(error);
-        this.$notify({type: 'error', text: error.response.data.message});
+        this.$notify({ type: "error", text: error.response.data.message });
       }
     },
     async sendUsername() {
       try {
         await Cytomine.instance.forgotUsername(this.email);
-        this.$notify({type: 'success', text: this.$t('notif-success-forgot-username')});
+        this.$notify({
+          type: "success",
+          text: this.$t("notif-success-forgot-username")
+        });
         this.forgotUsername = false;
-      }
-      catch(error) {
-        this.$notify({type: 'error', text: this.$t('notif-error-forgot-username')});
+      } catch (error) {
+        this.$notify({
+          type: "error",
+          text: this.$t("notif-error-forgot-username")
+        });
       }
     },
     async resetPassword() {
       try {
         await Cytomine.instance.forgotPassword(this.username);
-        this.$notify({type: 'success', text: this.$t('notif-success-forgot-password')});
+        this.$notify({
+          type: "success",
+          text: this.$t("notif-success-forgot-password")
+        });
         this.forgotPassword = false;
-      }
-      catch(error) {
-        this.$notify({type: 'error', text: this.$t('notif-error-forgot-password')});
+      } catch (error) {
+        this.$notify({
+          type: "error",
+          text: this.$t("notif-error-forgot-password")
+        });
       }
     }
   }
@@ -177,7 +232,8 @@ export default {
   margin-top: 3em;
 }
 
-.columns, .buttons {
+.columns,
+.buttons {
   margin-top: 0.5em;
 }
 
